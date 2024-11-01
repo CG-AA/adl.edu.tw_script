19eb5feb9e176e0190658e58d17bdad0b888fdd9
CG-TW <117447927+CG-AA@users.noreply.github.com>
2024-11-01 11:26:19 +0800

    support windows

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import subprocess
 from tqdm import tqdm
 import sys
+import os
 
 # Prompt the user for input
 count = int(input("Enter the number of times to loop: "))
@@ -10,8 +11,11 @@ suc = 0
 command = ""
 with open('cURL.txt', 'r') as file:
     command = file.read()
-    # sleep
-    command += f" && sleep {delay/1000};"
+    # sleep or timeout based on OS
+    if os.name == 'nt':  # Windows
+        command += f" && timeout /t {delay // 1000} /nobreak >nul"
+    else:  # Unix-based
+        command += f" && sleep {delay / 1000};"
 
 # Loop the specified number of times
 for i in tqdm(range(1, count + 1)):
@@ -19,8 +23,8 @@ for i in tqdm(range(1, count + 1)):
     # if message is {"msg":"\u7b54\u5c0d\u4e86\uff01"} add 1 to suc
     if res == '{"msg":"\\u7b54\\u5c0d\\u4e86\\uff01"}':
         suc += 1
-        print(str(suc)+'success')
+        print(str(suc) + ' success')
     else:
         print("\033[91mfailed, retrying\033[0m")
         print(res)
-        i-=1
+        i -= 1
